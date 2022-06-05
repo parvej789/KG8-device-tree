9d889ea2b50ec94d04f36eb8e908f9f097d68956
parvej789 <58601886+parvej789@users.noreply.github.com>
2022-06-05 14:15:17 +0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-KG8-user
-add_lunch_combo omni_TECNO-KG8-userdebug
-add_lunch_combo omni_TECNO-KG8-eng
+add_lunch_combo twrp_TECNO-KG8-user
+add_lunch_combo twrp_TECNO-KG8-userdebug
+add_lunch_combo twrp_TECNO-KG8-eng
